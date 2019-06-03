2ecedbade5516b519dab9542348c1e8b06d5ab80
Rachel Gonzalez <dancearchitect@gmail.com>
2019-06-03 18:09:48 -0400

    init React front end and cleaned house, linked React to Rails back end

--- feed_me/app/controllers/recipes_controller.rb
+++ feed_me/app/controllers/recipes_controller.rb
@@ -3,4 +3,34 @@ class RecipesController < ActionController::API
         @recipes = Recipe.all
         render json: @recipes, status: :ok
     end
+
+    def create 
+        @recipe = Recipe.new(recipe_params)
+
+        if @recipe.save
+            redirect_to recipe_path(@recipe)
+        else
+            render :new
+        end
+    end
+
+    def update
+        @recipe = recipe.find(params[:id])
+    
+        if @recipe.update(recipe_params)
+          render json: @recipe, status: :ok
+        else
+          render json: { errors: @recipe.errors }, status: :unprocessable_entity
+        end
+      end
+
+    def destroy
+        @recipe = Recipe.find(params[:id])
+        @recipe.destroy
+    end
+
+    private 
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :meal_image, :cuisine, :region, :cook_time, :ingredients, :measurements, :servings, :instructions, :user_id)
 end
