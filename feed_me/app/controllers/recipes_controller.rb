716ce4bf0504a3024db00b706ef5b9f13e60ec4a
Rachel Gonzalez <dancearchitect@gmail.com>
2019-06-03 17:07:37 -0400

    rendered all recipes through json

--- feed_me/app/controllers/recipes_controller.rb
+++ feed_me/app/controllers/recipes_controller.rb
@@ -1,7 +1,6 @@
 class RecipesController < ActionController::API
     def index
-        @user = User.find(params[:user_id])
-        @recipes = @user.recipes
-        render json: @recipes, include: :user, status: :ok
+        @recipes = Recipe.all
+        render json: @recipes, status: :ok
     end
 end
--- feed_me/config/routes.rb
+++ feed_me/config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  scope '/users/:user_id' do
     resources :recipes
-  end  
 end
